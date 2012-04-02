b8a37c1f50127ec834bd14ac77563bb00e837802
Damiano <damiano.giacomello@diginess.it>
2012-04-02 18:47:32 +0200

    fixed globalization permalink

--- app/models/spree/taxon_decorator.rb
+++ app/models/spree/taxon_decorator.rb
@@ -3,5 +3,13 @@ Spree::Taxon.class_eval do
   LANG = Spree::Config[:locales].split(",").collect {|l| l.to_sym} rescue I18n.available_locales
   globalize_accessors :locales => LANG, :attributes => [:name, :description, :permalink]
   
-  extend ::Globalize::Migratable
+  # Creates permalink based on Stringex's .to_url method
+  def set_permalink
+    if parent_id.nil?
+      self.permalink = name.to_url if self.permalink.blank?
+    else
+      parent_taxon = Spree::Taxon.find(parent_id)
+      self.permalink = [parent_taxon.permalink, (self.permalink.blank? ? name.to_url : self.permalink.split('/').last)].join('/')
+    end
+  end
 end

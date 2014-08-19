e9603ea86919a70cd436dda4d9b97a35f9f9d38d
Damiano <giacomello.damiano@gmail.com>
2014-08-19 14:36:43 +0200

    removed routing. update option_type model and view

--- app/models/spree/option_type_decorator.rb
+++ app/models/spree/option_type_decorator.rb
@@ -2,4 +2,6 @@ Spree::OptionType.class_eval do
   translates :name, :presentation
   attr_accessible :translations, :translations_attributes
   accepts_nested_attributes_for :translations
+
+  accepts_nested_attributes_for :option_values, :allow_destroy => false
 end
